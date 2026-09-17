603d0d220703161fcfd744535622d67f024988de
Tushar <tusharpramanick29@gmail.com>
2026-09-17 19:12:12 +0530

    add proper transitions

--- Accordion-component/script.js
+++ Accordion-component/script.js
@@ -1,9 +1,22 @@
 let accordions = document.querySelectorAll('.accordions');
+let allDescriptions = document.querySelectorAll('.description')
 
 accordions.forEach((element) => {
     element.addEventListener("click", () => {
         let article = element.parentElement;
-        let description = article.querySelector('.description');
-        description.classList.toggle("show");
+        let description = article.querySelector(".description");
+        let wasOpen = description.classList.contains("show");
+
+        allDescriptions.forEach((element) => {
+            if (element.classList.contains("show")) {
+                element.classList.remove("show")
+                element.parentElement.querySelector(".symbol").classList.remove("arrow")
+            }
+        })
+
+        if (!wasOpen) {
+            description.classList.add("show");
+            article.querySelector(".symbol").classList.add("arrow")
+        }
     });
 });
